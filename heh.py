2844928e3ad786614cdedfece0552728dc91d90e
Yakim126 <maxim.yakimaha@gmail.com>
2019-07-08 12:33:20 +0300

    Commit1

--- heh.py
+++ heh.py
@@ -2,10 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import telebot
 
-# python heh.py
-
-# 859071138:AAGbzXZ7cSfrQdUMxjaKBOA20VY7i_WgzbM
-# https://api.telegram.org/bot859071138:AAGbzXZ7cSfrQdUMxjaKBOA20VY7i_WgzbM/getMe
 bot = telebot.TeleBot('859071138:AAGbzXZ7cSfrQdUMxjaKBOA20VY7i_WgzbM');
 
 headers_Get = {
@@ -13,23 +9,23 @@ headers_Get = {
     }
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands = ['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
-    bot.send_message(message.chat.id, 'Я бот который найдет тебе все что ты хочешь!(в пределах Google)')
-    bot.send_message(message.chat.id, 'Введи команду /google ')
+    bot.send_message(message.chat.id, 'Hello, you wrote me a command: /start')
+    bot.send_message(message.chat.id, 'I ma a bot which find for you any query in Google search!')
+    bot.send_message(message.chat.id, 'Enter a command: /google')
 
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands = ['help'])
 def help_message(message):
-    bot.send_message(message.chat.id, 'Чтобы начать гуглить введи команду: /google ')
+    bot.send_message(message.chat.id, 'For search enter a command: /google ')
 
 
 def google(q):
     s = requests.Session()
     q = '+'.join(q.split())
     url = 'https://www.google.com/search?q=' + q
-    r = s.get(url, headers=headers_Get)
+    r = s.get(url, headers = headers_Get)
     soup = BeautifulSoup(r.text, "html.parser")
     output = []
     for search in soup.find_all('div', {'class':'r'}):
@@ -59,12 +55,13 @@ def ifLen10(url, output):
 
 @bot.message_handler(commands=['google'])
 def messageSearch(message):
-    send = bot.send_message(message.chat.id, 'Введи запрос')
+    send = bot.send_message(message.chat.id, 'Enter the query!')
     bot.register_next_step_handler(send, heh)
+
 
 def heh(message):
     for i in range(10):
-        bot.send_message(message.chat.id, 'Сайт номер: ' + str(i+1))
+        bot.send_message(message.chat.id, 'Website number: ' + str(i+1))
         bot.send_message(message.chat.id, google(message.text)[i])
         i += 1
 
