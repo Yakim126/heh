f11872c1068aab1dce85633b8e32c9241f2c8050
Yakim126 <maxim.yakimaha@gmail.com>
2019-07-23 10:09:22 +0300

    Commit1

--- heh.py
+++ heh.py
@@ -2,7 +2,6 @@ import requests
 import telebot
 from bs4 import BeautifulSoup
 
-
 bot = telebot.TeleBot('859071138:AAGbzXZ7cSfrQdUMxjaKBOA20VY7i_WgzbM');
 headers_Get = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
